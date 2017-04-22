6898ca2d4499d88aca46c58b7882231cb2f6378a
Victor Hsiao <vwhsiao@uci.edu>
2017-04-22 14:51:52 -0700

    - Animation Idle and Run for Player Square - Experimental Glue Code

---
+++ World of Edge/Assets/Scripts/GlueTestScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlueTestScript : MonoBehaviour {
+
+    GameObject target;
+    GameObject[] targetList;
+    // Use this for initialization
+    void Start () {
+        targetList = GameObject.FindGameObjectsWithTag("Enemy");
+        target = targetList[0].gameObject == gameObject ? targetList[1] : targetList[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.parent == null)
+        {
+            transform.position =Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime * 5.0f);
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.parent== null && collision.transform.tag == "Enemy")
+        {
+            collision.transform.parent= transform;
+            collision.gameObject.tag = "EnemyComponent";
+            if (collision.gameObject == target)
+            {
+                changeTargets();
+            }
+        }
+
+    }
+
+    private void changeTargets()
+    {
+        targetList = GameObject.FindGameObjectsWithTag("Enemy");
+        target = targetList[0].gameObject == gameObject ? targetList[1] : targetList[0];
+    }
+}
