b25b08135e6a626379e247a830dd4996bebbd706
Victor Orozco <onceadayeveryday@gmail.com>
2017-04-22 15:48:41 -0700

    Add gluer prefab

---
+++ World of Edge/Assets/Scripts/GluerAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GluerAI : MonoBehaviour {
+    NavMeshAgent agent;
+    GameObject player;
+    List<GameObject> enemies;
+
+    public EnemyManager enemyManager;
+
+
+
+	void Start () {
+        enemies = enemyManager.enemyList;
+        agent = GetComponent<NavMeshAgent>();
+        moveToClosest();
+	}
+	
+    void moveToClosest() {
+        GameObject closest = findClosest();
+        agent.destination = closest.transform.position;
+    }
+
+    GameObject findClosest() {
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+
+        foreach(GameObject enemy in enemies) {
+            float currDistance = Vector3.Distance(position, enemy.transform.position);
+            if (currDistance < distance) {
+                distance = currDistance;
+                closest = enemy;
+            }
+        }
+        return closest;
+    }
+
+}
