7df14dfbae540128b83077f48c43985cc9356d22
Kyle Wong <kylewonga@gmail.com>
2017-04-22 15:15:12 -0700

    Added CameraFollow script

---
+++ World of Edge/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    // Use this for initialization
+    private float Frac;
+    private Transform playerTransform;
+    public float yOffSet;
+	void Start () {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        Frac = 1f / 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        move();
+	}
+    private void move()
+    {
+        transform.position += (playerTransform.position+Vector3.up*yOffSet - transform.position) * Frac * (1 - Time.deltaTime);
+
+    }
+}
