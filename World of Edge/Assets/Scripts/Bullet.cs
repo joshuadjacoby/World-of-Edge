6f88f21d4ade9619080884f206aa1c2fdc11fd68
Josh Jacoby <joshuadjacoby@gmail.com>
2017-04-22 12:26:19 -0700

    Added working Bullet script with constant speed

---
+++ World of Edge/Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Vector3 shootDirection = Input.mousePosition;
+        shootDirection = Camera.main.ScreenToWorldPoint(shootDirection);
+        shootDirection = shootDirection - transform.position;
+        shootDirection.z = 0.0f;
+        shootDirection = shootDirection.normalized;
+
+        gameObject.GetComponent<Rigidbody>().AddForce(shootDirection * 1000);
+       
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (!gameObject.GetComponent<Renderer>().isVisible)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ World of Edge/Assets/Scripts/BulletSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawn : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            GameObject go = Instantiate(Resources.Load("Prefabs/Candy"), gameObject.transform.position, Quaternion.identity) as GameObject;
+        }
+    }
+}
