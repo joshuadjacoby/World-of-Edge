abd9336e57e92b754de7515f1360e41bcee91d90
Ted Won <tkwon09@gmail.com>
2017-04-21 22:52:42 -0700

    Basic player acceleartion and navmesh stuff

---
+++ World of Edge/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public float moveAcceleration;
+    public float maxMoveSpeed;
+    public float friction;
+
+    public void Update()
+    {
+        Vector3 moveDir = new Vector3();
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveDir += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveDir += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDir += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveDir += Vector3.right;
+        }
+
+        navMeshAgent.velocity += moveDir * moveAcceleration * Time.deltaTime;
+        if (navMeshAgent.velocity.magnitude > maxMoveSpeed)
+        {
+            navMeshAgent.velocity = navMeshAgent.velocity.normalized * maxMoveSpeed;
+        }
+        transform.rotation = Quaternion.Euler(90, 0, 0);
+    }
+}
