bc6b50ac61abea5e737cdb7e7f64d3b363f51d4f
Victor Orozco <onceadayeveryday@gmail.com>
2017-04-23 11:18:48 -0700

    Add bullet Parent class and unfinished grenade shot.

---
+++ World of Edge/Assets/Scripts/BulletParent.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletParent : MonoBehaviour {
+    public float lifeTimer;
+    public float speed;
+    public float damage;
+    public Vector3 direction;
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (lifeTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            transform.position += direction * speed * Time.fixedDeltaTime;
+            lifeTimer -= Time.fixedDeltaTime;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Health health = collision.gameObject.GetComponent<Health>();
+        if (health != null)
+        {
+            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.Flash();
+            }
+            health.takeDamage((int)damage);
+            Destroy(gameObject);
+        }
+        else
+        {
+            if (collision.gameObject.tag == "Wall")
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ World of Edge/Assets/Scripts/GrenadeShot.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeShot : MonoBehaviour {
+    public float lifeTimer;
+    public float speed;
+    public float damage;
+    public Vector3 direction;
+
+    public float blastRadius;
+
+    void FixedUpdate () {
+        if (lifeTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            transform.position += direction * speed * Time.fixedDeltaTime;
+            lifeTimer -= Time.fixedDeltaTime;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) 
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, blastRadius);
+        foreach(Collider hit in hitColliders) {
+            Health health = collision.gameObject.GetComponent<Health>();
+            if (health != null)
+            {
+                Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+                if (enemy != null)
+                {
+                    enemy.Flash();
+                }
+                health.takeDamage((int)damage);
+                Destroy(gameObject);
+            }
+            else
+            {
+                if (collision.gameObject.tag == "Wall")
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
