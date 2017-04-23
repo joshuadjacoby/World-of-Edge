5d9d50c2f9771ae0defeaaf3aff1912bcc76b7e5
Victor Orozco <onceadayeveryday@gmail.com>
2017-04-22 19:11:22 -0700

    Added ability to give weights to prefabs.
    No errorchecking for probabilities. Have to edit 2 arrays in editor.

--- World of Edge/Assets/Scripts/EnemyManager.cs
+++ World of Edge/Assets/Scripts/EnemyManager.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour {
 
+    struct RandomSelection {
+        private int enemyIndex;
+        public float probability;
+     
+        public RandomSelection(int enemyIndex, float probability) {
+            this.enemyIndex = enemyIndex;
+            this.probability = probability;
+        }
+        public int GetValue() { return enemyIndex; }
+    }
+     
+
+
     public GameObject[] enemyPrefabs;
     public List<GameObject> enemyList;
 
+    public float[] probabilites;
+    float upperProbability;
 
     //modify these to actual boundaries
     float minX =-5.0f;
@@ -17,35 +32,58 @@ public class EnemyManager : MonoBehaviour {
     public float spawnInterval;
     public float intervalVariance;
     public float spawnDelay;
+    public int waveSize;
     //object with animation to run
     public GameObject spawnAnimation;
+    RandomSelection[] randomSelections;
 
     void Start() {
+        //Stores probability weights for usage with getRandomEnemyIndex();
+        randomSelections = new RandomSelection[enemyPrefabs.Length];
+        for (int i = 0;i < enemyPrefabs.Length;i++){
+            randomSelections[i] = new RandomSelection(i, probabilites[i]);
+        }
+
         //grabs prefabs from "Assets/Enemies"
         enemyList = new List<GameObject>();
-        StartCoroutine("spawnEnemy");
+        StartCoroutine(spawnEnemies(waveSize,spawnInterval));
     }
 
-    IEnumerator spawnEnemy() {
-        while (true) {
+    IEnumerator spawnEnemies(int waveSize, float spawnInterval) {
+        while (enemyList.Count < waveSize) {
             //set conditions for stopping spawner when player is dead
-            GameObject enemy = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+            int enemyIndex = GetRandomEnemyIndex(randomSelections);
             Vector3 position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0);
             //instantiate animation at position with SpawnDelay using coroutine
             //StartCoroutine(waitAnimation(spawnDelay, position));
 
-            GameObject toAdd = Instantiate(enemy, position, Quaternion.identity);
+            GameObject toAdd = Instantiate(enemyPrefabs[enemyIndex], position, Quaternion.identity);
             toAdd.GetComponent<Enemy>().enemyManager = this;
             enemyList.Add(toAdd);
 
             yield return new WaitForSeconds(Random.Range(spawnInterval - intervalVariance, spawnInterval + intervalVariance));
         }
+        //end the coroutine
 
     }
 
     IEnumerator waitAnimation(float waitTime, Vector3 position) {
         Instantiate(spawnAnimation, position, Quaternion.identity);
         yield return new WaitForSeconds(waitTime);
+    }
+
+    int GetRandomEnemyIndex(params RandomSelection[] selections) {
+        float rand = Random.value;
+        float currentProb = 0;
+        foreach (RandomSelection selection in selections) {
+            currentProb += selection.probability;
+            if (rand <= currentProb)
+                return selection.GetValue();
+        }
+     
+        //will happen if the input's probabilities sums to less than 1
+        //throw error here if that's appropriate
+        return -1;
     }
 
 }
