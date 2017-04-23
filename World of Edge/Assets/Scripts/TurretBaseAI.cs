2b0991e15ec8fc497cbe8222ab9f11874454f085
Ted Won <tkwon09@gmail.com>
2017-04-22 18:20:26 -0700

    Renamed Shooter to Turretbase, turrets now move towards closest TurretBase. Will simplify

--- World of Edge/Assets/Scripts/TurretBaseAI.cs
+++ World of Edge/Assets/Scripts/TurretBaseAI.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class ShooterAI : Enemy {
+public class TurretBaseAI : Enemy {
 
     // Use this for initialization
     public float shootRadius;
