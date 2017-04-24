275bba2d1f0472ad8ea21ffb2e00dde5bd5a644c
Kyle Wong <kylewonga@gmail.com>
2017-04-23 18:21:38 -0700

    Added new cursor

---
+++ World of Edge/Assets/SetCursor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCursor : MonoBehaviour {
+
+    // Use this for initialization
+    public Texture2D texture;
+    public Vector2 offSet;
+	void Start () {
+        Cursor.SetCursor(texture, offSet, CursorMode.Auto);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Cursor.SetCursor(texture, offSet, CursorMode.Auto);
+    }
+}
