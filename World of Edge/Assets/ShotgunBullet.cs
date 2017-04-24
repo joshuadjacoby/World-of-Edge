556793466c13cf7f4dd0ca141a9f81f1ca69db46
Kyle Wong <kylewonga@gmail.com>
2017-04-23 21:30:29 -0700

    Added shotgun

---
+++ World of Edge/Assets/ShotgunBullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunBullet : BulletParent {
+
+    // Use this for initialization
+    public float deceleration;
+    private Vector3 decelVector;
+    private Rigidbody rb;
+	void Start () {
+        move();
+        rb = GetComponent<Rigidbody>();
+        decelVector = rb.velocity * -1 * deceleration;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        updateLifeTime();
+        rb.velocity += decelVector * Time.deltaTime;
+	}
+}
