a85fc89ab1ac9cbc2c968715891eeef95b8afeaf
Kyle Wong <kylewonga@gmail.com>
2017-04-23 19:15:32 -0700

    Sin bullets

---
+++ World of Edge/Assets/Scripts/SinBullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinBullet : BulletParent {
+
+    // Use this for initialization
+    private float orthoVelocity;
+    public float oscilliationSpeed;
+    public float oscillationMagnitude;
+    private float timer;
+	void Start () {
+        orthoVelocity = 0;
+        timer = 0;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        timer += oscilliationSpeed * Time.deltaTime;
+        orthoVelocity = oscillationMagnitude * Mathf.Cos(timer);
+        GetComponent<Rigidbody>().velocity = direction * speed + orthoVelocity*transform.right;
+        updateLifeTime();
+        
+	}
+}
